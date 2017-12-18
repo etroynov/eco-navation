2d53891f5fbacf90e7455b13d4a1663e0a15bd86
Evgeniy Troynov <etroynov@gmail.com>
2017-12-18 15:11:38 +0500

    #master. update medical courses.

--- src/ts/components/site/courses/List.tsx
+++ src/ts/components/site/courses/List.tsx
@@ -1,23 +1,75 @@
 import * as React from 'react';
-import { Row } from 'antd';
+import { Row, Tag } from 'antd';
+
+/**
+ * Components
+ */
 
 import Item from './Item';
 import Container from './../common/Container';
 
+const { CheckableTag } = Tag;
 
-const List = ({ name, description, items } : { name: string; description: string; items: any[] }) => (
-  <Container>
-    <section className="courses">
-    <header className="courses__header">
-      <h2 className="courses__title">{name}</h2>
-      <p className="courses__description">{description}</p>
-    </header>
+/*!
+ * Expos
+ */
 
-    <Row>
-      {items.map(item => <Item {...item} />)}
-    </Row>
-    </section>
-  </Container>
-);
+class List extends React.Component<{ name: string; description: string; items: any[], tagsCloud?: any[] }, {}> {
+  state = {
+    selectedTags: [],
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = (tag, checked) => {
+    const { selectedTags } = this.state;
+
+    const nextSelectedTags = checked ?
+            [...selectedTags, tag] :
+            selectedTags.filter(t => t !== tag);
+
+    console.log('You are interested in: ', nextSelectedTags);
+
+    this.setState({ selectedTags: nextSelectedTags });
+  }
+
+  render() {
+    const { name = '', description = '', items = [], tagsCloud = [] } = this.props;
+    const { selectedTags } = this.state;
+    
+    let currentItems = items;
+
+    if (!!selectedTags.length) { currentItems = items.filter(item => selectedTags.includes(item.tags)); }
+
+    return (
+      <Container>
+        <section className="courses">
+        <header className="courses__header">
+          <h2 className="courses__title">{name}</h2>
+          <p className="courses__description">{description}</p>
+          <div className="tags-cloud">
+            {
+              tagsCloud.map(({ name, slug }) => <CheckableTag 
+                key={slug}
+                className="tags-cloud__item"
+                checked={selectedTags.includes(slug)}
+                onChange={checked => this.handleChange(slug, checked)} 
+              >
+                {name}
+              </CheckableTag>)
+            }
+          </div>
+        </header>
+
+        <Row>
+          {currentItems.map(item => <Item key={item.id} {...item} />)}
+        </Row>
+        </section>
+      </Container>
+    );
+  }
+}
 
 export default List;
